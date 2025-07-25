01aed543643afb41c5630172e5cb2e9e45aaf06d
shrunkh0310 <work.shrunhalmore@gmail.com>
2025-07-25 19:32:54 +0530

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    try {
+        mongoose.connection.on('connected', ()=>console.log("db connected"));
+        await mongoose.connect(`${process.env.MONGODB_URI}/carrental`)
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+}
+
+export default connectDB
